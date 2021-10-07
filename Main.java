b357a0c2fa748b6b1ae38418a39f12d7c495ca83
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2021-10-07 15:05:37 +0000

    Added test.class to see functionality

--- Main.java
+++ Main.java
@@ -5,11 +5,11 @@ import javax.swing.SwingConstants;
 class Main {
   public static void main(String[] args) {
     JFrame frame = new JFrame("Hello world");
-    frame.setSize(300, 300);
+    frame.setSize(375, 375);
     frame.setLocation(100, 100);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JLabel label = new JLabel("Hello world", SwingConstants.CENTER);
+    JLabel label = new JLabel(test.message(), SwingConstants.CENTER);
     frame.add(label);
 
     frame.show();
---
+++ test.java
@@ -0,0 +1,5 @@
+public class test extends Main{
+  public static String message(){
+    return "does this class connect?";
+  }
+}
